345b98179b9b7532301dc532139eec3e704806c3
Scott Breitenbach <scott.breitenbach@gmail.com>
2021-10-03 18:57:35 -0400

    returning all starts for a letter

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 function search_grid_for_start(grid, first_letter)
- {
+ {  
+    //TODO - this will return every starting point
+    //Rework to find and test one at a time 
+    var results = [];
     for (var x in grid) {
         var row = grid[x]
         for (var y in row) {
             var letter = row[y]
             if (letter == first_letter) { 
-                //console.log(letter + x + y)
-                return ([x,y])
+                results.push([x,y])
              };
         }
     };
+    return results
  };
 
+function check_for_match(word,start)
+{
+    //can initially go up, down, left, right, and diagonal
+    //letters must be sequential (but can be spelled in reverse)
+    //all letters must follow the same direction
+    //cannot search outside the bounds of the grid or "jump" from the end of one side to the start of the other 
+    return
+}
 var search_grid = [
     ["F", "B", "H", "D"],
     ["E", "U", "I", "H"],
@@ -25,6 +36,6 @@ for (var i in words) {
     console.log(words[i])
     var word = words[i]
     var first_letter = word.charAt(0)
-    var start = search_grid_for_start(search_grid,first_letter)
-    console.log(start)
+    var starts = search_grid_for_start(search_grid,first_letter)
+    //TODO look in adjacent spot for matches
 };
